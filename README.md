5bfc106c210155c8a7308a6fa1d41636d757be2c
Agentii <56613955+JacobHMartens@users.noreply.github.com>
2023-10-11 15:59:48 +0200

    workspace setup (testing environment)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # BachelorProject
 Bachelor Project - Building a tool for analysing unsafe code in Rust
 
-
+## Install rust compiler library
 git clone https://github.com/rust-lang/rust.git
 
 cd rust
@@ -11,3 +11,8 @@ Open powershell:
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 ./x build
+
+## Set workspace environment to use Nightly
+rustup toolchain install nightly
+
+rustup override set nightly
--- test_package/src/main.rs
+++ test_package/src/main.rs
@@ -7,9 +7,22 @@ run_compiler creates a Compiler [*2] from the configuration and passes it to the
 Inside the closure, you can use the Compiler to drive queries to compile a crate and get the results.
 This is what the rustc_driver does too. You can see a minimal example of how to use rustc_interface here [*3].
 
+You can see what queries are currently available through the rustdocs for Compiler [*4].
+You can see an example of how to use them by looking at the rustc_driver implementation, specifically the
+rustc_driver::run_compiler [*5] function (not to be confused with rustc_interface::run_compiler [*6]).
+The rustc_driver::run_compiler function takes a bunch of command-line args and some other configurations
+and drives the compilation to completion.
+
+rustc_driver::run_compiler also takes a Callbacks [*7], a trait that allows for custom compiler configuration,
+as well as allowing some custom code run after different phases of the compilation.
+
 [*1] https://doc.rust-lang.org/nightly/nightly-rustc/rustc_interface/interface/fn.run_compiler.html
 [*2] https://doc.rust-lang.org/nightly/nightly-rustc/rustc_interface/interface/struct.Compiler.html
 [*3] https://github.com/rust-lang/rustc-dev-guide/blob/master/examples/rustc-driver-example.rs
+[*4] https://doc.rust-lang.org/nightly/nightly-rustc/rustc_interface/interface/struct.Compiler.html
+[*5] https://doc.rust-lang.org/nightly/nightly-rustc/rustc_driver_impl/fn.run_compiler.html
+[*6] https://doc.rust-lang.org/nightly/nightly-rustc/rustc_interface/interface/fn.run_compiler.html
+[*7] https://doc.rust-lang.org/nightly/nightly-rustc/rustc_driver/trait.Callbacks.html
 */
 
 extern crate rustc_driver;
